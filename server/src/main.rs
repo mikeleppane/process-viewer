7ba98449fc2e7f6950293257ef3c34a392c49d98
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-03-03 18:18:14 +0200

    Added api/memory endpoint, creating a trait to transform memory values into a human-readable form.

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,12 +5,33 @@ use axum::{Json, Router, Server};
 use axum_macros::debug_handler;
 use serde::{Deserialize, Serialize};
 use std::env;
-use std::fmt::Write;
 use std::sync::Arc;
-use sysinfo::{Cpu, CpuExt, System, SystemExt};
+use sysinfo::{CpuExt, System, SystemExt};
 use tokio::sync::Mutex;
 
 const DEFAULT_PORT: u16 = 7070;
+
+trait HumanReadable: Sized {
+    fn to_human(self) -> String;
+}
+
+impl HumanReadable for u64 {
+    fn to_human(self) -> String {
+        match self {
+            0..=999 => self.to_string(),
+            1000..=999_999 => {
+                format!("{:.2} KB", self as f64 / 1000f64)
+            }
+            1_000_000..=999_999_999 => {
+                format!("{:.2} MB", self as f64 / 1_000_000f64)
+            }
+            1_000_000_000.. => {
+                format!("{:.2} GB", self as f64 / 1_000_000_000f64)
+            }
+        }
+    }
+}
+
 
 fn get_address() -> String {
     let port = env::var("PORT").unwrap_or(DEFAULT_PORT.to_string());
@@ -20,6 +41,7 @@ fn get_address() -> String {
 fn router() -> Router {
     Router::new()
         .route("/api/cpus", get(get_cpus))
+        .route("/api/memory", get(get_memory))
         .route("/health", get(health))
         .with_state(AppState {
             sys: Arc::new(Mutex::new(System::new())),
@@ -46,7 +68,15 @@ struct CpuInfo {
     cpu_usage: f32,
     frequency: u64,
     vendor_id: String,
-    brand: String,
+    brand: String
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Memory {
+    total_memory: String,
+    used_memory: String,
+    total_swap: String,
+    used_swap: String
 }
 
 #[debug_handler]
@@ -65,6 +95,20 @@ async fn get_cpus(State(state): State<AppState>) -> impl IntoResponse {
         })
         .collect();
     Json(cpus)
+}
+
+#[debug_handler]
+async fn get_memory(State(state): State<AppState>) -> impl IntoResponse {
+    let mut sys = state.sys.lock().await;
+    sys.refresh_memory();
+
+    let memory = Memory {
+        total_memory: sys.total_memory().to_human(),
+        used_memory: sys.used_memory().to_human(),
+        total_swap: sys.total_swap().to_human(),
+        used_swap: sys.used_swap().to_human(),
+    };
+    Json(memory)
 }
 
 #[debug_handler]
